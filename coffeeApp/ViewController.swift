270580f54a8581c9975d426496e1af99e9c19b98
tinpail720 <tmitchell720@icloud.com>
2016-11-30 16:59:53 -0700

    Initial Commit

---
+++ coffeeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coffeeApp
+//
+//  Created by TIM MITCHELL on 11/30/16.
+//  Copyright © 2016 TinPailSoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
